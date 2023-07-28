4fa0223b8b2b4b29067f4024d2ca684f69ad1704
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-28 12:44:53 -0700

    touching up header file

--- shell.h
+++ shell.h
@@ -1,18 +1,16 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-/** LIBRARIES USED */
+/**=============================*/
+/**====== LIBRARIES USED =======*/
+/**=============================*/
 
-#include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <string.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <linux/limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #endif
