6e39557e7d57749144d4f100c7128958386fdecb
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-28 12:39:54 -0700

    created shell.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/** LIBRARIES USED */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
+#endif
