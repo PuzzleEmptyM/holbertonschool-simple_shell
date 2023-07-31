08b716043d96c287344fa3b84c33e93755cf06c2
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-30 21:27:47 -0700

    shell should now handle the "end of file" condition. also uses execv to execute commands, ensuring that it doesn't rely on the PATH for finding executables

--- loop-y_function.c
+++ loop-y_function.c
@@ -12,25 +12,30 @@ int main() {
     char* args[MAX_ARGS];
     int status;
     pid_t pid;
-    int argCount;
-    char* token;
+    int argCount;  /* Moved declaration here */
+    char* token;   /* Moved declaration here */
 
     while (1) {
         printf("SimpleShell> ");
         fflush(stdout); /* Make sure the prompt is displayed before reading input */
-        fgets(input, sizeof(input), stdin);
+
+        // Check if Ctrl+D (EOF) is encountered
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
 
         /* Remove the newline character from the input */
         input[strcspn(input, "\n")] = '\0';
 
         /* Tokenize the input into separate arguments */
         token = strtok(input, " ");
-        argCount = 0;
+        argCount = 0;  /* Initializing here */
         while (token != NULL && argCount < MAX_ARGS - 1) {
             args[argCount++] = token;
             token = strtok(NULL, " ");
         }
-        args[argCount] = NULL; /* The last argument must be NULL for execvp */
+        args[argCount] = NULL; /* The last argument must be NULL for execv */
 
         /* Check for the exit command */
         if (strcmp(args[0], "exit") == 0) {
@@ -43,18 +48,13 @@ int main() {
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
             /* Child process */
-            if (execvp(args[0], args) == -1) {
-                perror("execvp");
+            if (execv(args[0], args) == -1) {
+                perror("execv");
                 exit(EXIT_FAILURE);
             }
         } else {
             /* Parent process */
             wait(&status);
-
-            /* Print the command's output (you can redirect to file or another stream if desired) */
-            printf("\nCommand Output:\n");
-            fflush(stdout);
-            system(input); /* Executes the same command using the system() function */
 
             /* Wait for the user to press Enter before displaying the prompt again */
             printf("\nPress Enter to continue...");
